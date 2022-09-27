3f5f13e5932fd111f780901d80c057995896b318
abedinforhane <farhanengn@gmail.comm>
2022-09-27 20:58:50 +0600

    basic project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Topbar from "./components/Topbar/Topbar";
+
+function App() {
+  return (
+   <div className="topbar">
+     <Topbar/>
+   </div>
+  );
+}
+export default App;
